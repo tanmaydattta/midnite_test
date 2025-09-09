8bb6b36e14fd73bf95cd5ad3998a33cfa2429887
tanmay <tanmay@Mac-mini.local>
2025-09-10 09:25:02 +1000

    fix test

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,11 +7,17 @@ def client():
     with app.test_client() as client:
         yield client
 
+@pytest.fixture(autouse=True)
+def clear_store():
+    # Clear DB before and after each test
+    store.clear()
+    yield
+    store.clear()
+
 def post_event(client, payload):
     return client.post("/event", json=payload)
 
 def test_deposit_over_200_in_30s(client):
-    store._data.clear()
     user_id = 1
     post_event(client, {"type": "deposit", "amount": "150.00", "user_id": user_id, "t": 0})
     rv = post_event(client, {"type": "deposit", "amount": "100.00", "user_id": user_id, "t": 10})
@@ -19,14 +25,12 @@ def test_deposit_over_200_in_30s(client):
     assert 123 in data["alert_codes"]
 
 def test_withdraw_over_100(client):
-    store._data.clear()
     user_id = 2
     rv = post_event(client, {"type": "withdraw", "amount": "150.00", "user_id": user_id, "t": 0})
     data = rv.get_json()
     assert 1100 in data["alert_codes"]
 
 def test_three_consec_withdraws(client):
-    store._data.clear()
     user_id = 3
     post_event(client, {"type": "withdraw", "amount": "10.00", "user_id": user_id, "t": 0})
     post_event(client, {"type": "withdraw", "amount": "20.00", "user_id": user_id, "t": 1})
@@ -35,7 +39,6 @@ def test_three_consec_withdraws(client):
     assert 30 in data["alert_codes"]
 
 def test_three_increasing_deposits(client):
-    store._data.clear()
     user_id = 4
     post_event(client, {"type": "deposit", "amount": "10.00", "user_id": user_id, "t": 0})
     post_event(client, {"type": "deposit", "amount": "20.00", "user_id": user_id, "t": 1})
